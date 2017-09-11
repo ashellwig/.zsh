cbb6cba84ac103994e54cd4bfc163653bbd427c4
ashellwig <hellwigashton@gmail.com>
2017-09-10 22:10:12 -0600

    adding odules

--- functions.d/short_functions.zsh
+++ functions.d/short_functions.zsh
@@ -7,7 +7,21 @@
 
 # Perform a Full Backup with Rsync
 function full_backup() {
+  if [[ -e /mnt/FULL_BACKUP/swapfile ]] ; then
+    print -f "[1;31m\nBackup dir is full!"
+    sudo rm -rf /mnt/FULL_BACKUP
+  else
+    print -f "[1;33m\nBackup dir found!"
+    return 0
+  fi
+  print -f "[3;33m\nMounting..[0m"
+  sudo mount /dev/sdb2 /mnt/FULL_BACKUP
+  print -f "[3;33m\nExecuting backup command...[0m"
   sudo rsync '-aAX' '--info=progress2' '--stats' '--delete' "--exclude=/home/ahellwig/*" "--exclude=/dev/*" "--exclude=/proc/*" "--exclude=/sys/*" "--exclude=/tmp/*" "--exclude=/run/*" "--exclude=/mnt/*" "--exclude=/media/*" "--exclude=/lost+found/*" / /mnt/FULL_BACKUP
+  print -f "[3;33m\nUnmounting device...[0m"
+  sudo umount --force /dev/sdb2
+  print -f "[1;32m\nBackup Complete![0m"
+
 }
 
 # Create a New Executable File
@@ -49,3 +63,5 @@ function updateparity {
   cargo install
   return 0
 }
+
+# vim: set et ts=2 sw=2:
