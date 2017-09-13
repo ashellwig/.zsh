028c81e41e86289c5a2366c1d66d90861e584fda
ashellwig <hellwigashton@gmail.com>
2017-09-12 23:58:06 -0600

    modified short functions

--- functions.d/short_functions.zsh
+++ functions.d/short_functions.zsh
@@ -7,13 +7,6 @@
 
 # Perform a Full Backup with Rsync
 function full_backup() {
-  if [[ -e /mnt/FULL_BACKUP/swapfile ]] ; then
-    print -f "[1;31m\nBackup dir is full!"
-    sudo rm -rf /mnt/FULL_BACKUP
-  else
-    print -f "[1;33m\nBackup dir found!"
-    return 0
-  fi
   print -f "[3;33m\nMounting..[0m"
   sudo mount /dev/sdb2 /mnt/FULL_BACKUP
   print -f "[3;33m\nExecuting backup command...[0m"
@@ -21,7 +14,6 @@ function full_backup() {
   print -f "[3;33m\nUnmounting device...[0m"
   sudo umount --force /dev/sdb2
   print -f "[1;32m\nBackup Complete![0m"
-
 }
 
 # Create a New Executable File
@@ -45,6 +37,11 @@ function makenewdir() {
   print "Please name the directory to be created."
   print "If it already exists, the command will exit."
   read newdirmade
+  if [[ ! "$#" -eq 0 ]]; then
+    local newdirmade="$1"
+  elif [[ "$#" -eq 0 ]]; then
+    return
+  fi
   if [[ ! -d ${newdirmade} ]]; then
     mkdir -p ${newdirmade}
     cd ${newdirmade}
