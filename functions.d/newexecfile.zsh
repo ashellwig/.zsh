90c370516b5794d0ff9cc076b1c48db919a8b28a
ashellwig <hellwigashton@gmail.com>
2017-09-17 03:20:34 -0600

    updated func

--- functions.d/newexecfile.zsh
+++ functions.d/newexecfile.zsh
@@ -3,5 +3,31 @@
 
 function newexecfile() {
   while [[ $# -gt 1 ]]; do
-    local key="$1"
+    key="$1"
+
+    case $key in
+      -n|--name)
+        local filenamein="$2"
+        shift
+        ;;
+      -d|--directory)
+        local directoryloc="$2"
+        shift
+        ;;
+      -h|--help)
+        local helpmsg="$(cat <<END
+Option                    Purpose
+-------------------------------------------------
+-n     --name             filename to save as
+-d     --directory        Directory to save to
+-h     --help             show this help and exit
+END
+)"
+        shift
+        ;;
+    esac
+  shift
+  done
 }
+
+# vim: set et ft=zsh ts=2 sw=2:
