d5e79cea4f4afd3675ed0cacdd36cf220aef77e7
ashellwig <hellwigashton@gmail.com>
2017-09-18 02:00:33 -0600

    updated func

--- functions.d/newexecfile.zsh
+++ functions.d/newexecfile.zsh
@@ -5,7 +5,8 @@ newexecfile() {
   local OPTIND=1
   local output_fname=""
   local output_dirname=""
-  local helpmessage="$(cat <<END
+  local helpmessage="$(
+    cat <<END
 \033[1mPurpose:  \033[0mThis function creates a new executable file.
 \033[1mMaintained by:  \033[0;32mAshton Hellwig \033[0;4m<hellwigasthon@gmail.com>
 \033[0;1mUsage: \033[0m newexecfile -f <filename> [-d <directory>] [-h]
@@ -16,28 +17,31 @@ Option                         Purpose
 -d         --directory        Directory to save to
 -h         --help             show this help and exit
 END
-)"
+  )"
 
   while getopts "h,f:,d:" opt; do
     case "$opt" in
-      h|\?)
-        echo "${helpmessage}"
-        ;;
-      f)
-        local output_fname=$OPTARG
-        shift 2
-        ;;
-      d)
-        local output_dirname=$OPTARG
-        shift
-        ;;
-      *)
-        ;;
+    h)
+      echo "${helpmessage}"
+      ;;
+    f)
+      local output_fname=$OPTARG
+      shift 2
+      ;;
+    d)
+      local output_dirname=$OPTARG
+      shift
+      ;;
+    *)
+      printf "\033[1;31mFilename \033[0m(-f) is \033[4;31mrequired\033[0m\n"
+      echo -e "$helpmessage\c"
+      return 1
+      ;;
     esac
   done
 
-  shift $((OPTIND-1))
-  [[ "$1" = "--" ]] && shift
+  shift $((OPTIND - 1))
+  [[ "$1" == "--" ]] && shift
 
   if [[ -n "$output_fname" ]]; then
     if [[ -n "$output_dirname" ]]; then
@@ -45,8 +49,8 @@ END
         print -f "\033[1;31mCannot create dir.\033[0m"
       elif [[ ! -d $output_dirname ]]; then
         mkdir -p "${output_dirname}" &&
-        touch "${output_dirname}/${output_fname}" ;
-        chmod +x "${output_dirname}/${output_fname}" ;
+          touch "${output_dirname}/${output_fname}"
+        chmod +x "${output_dirname}/${output_fname}"
         printf "\033[1;32mSuccess!\033[0m (new dir made)"
         return 0
       fi
@@ -56,7 +60,7 @@ END
         chmod +x $output_fname
         printf "\033[1;32mSuccess!\033[0m (no new dir)"
         return 0
-      elif [[ -e "$output_fname" ]]; then
+      elif [[ -e "${output_fname}" ]]; then
         printf "\033[1;31mFile exists!\033[0m"
         return 1
       fi
