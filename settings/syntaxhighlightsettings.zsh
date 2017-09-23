7fe6d31a22acd9b6019ded4f946763ada6da0c3f
ashellwig <hellwigashton@gmail.com>
2017-09-23 00:43:00 -0600

    updated func

--- settings/syntaxhighlightsettings.zsh
+++ settings/syntaxhighlightsettings.zsh
@@ -2,12 +2,12 @@ if [[ -d ${ZDOTDIR}/functions/zsh-syntax-highlighting ]]; then
   . ${ZDOTDIR}/functions/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern line cursor root)
   ZSH_HIGHLIGHT_PATTERNS+=(
-    'rm -rf *' 'fg=white,bold,bg=red' \
+    'rm -rf' 'fg=white,bold,bg=red' \
     'killall *' 'fg=white,bold,bg=red' \
-    'pkill*-HUP *' 'fg=black,bold,bg=yellow' \
-    'pacman*-S *' 'fg=white,bold,bg=green' \
-    'touch *' 'fg=white,bold,bg=green' \
-    'makenewdir*' 'fg=white,bold,bg=green' \
+    'pkill -HUP' 'fg=black,bold,bg=yellow' \
+    'pacman*-S' 'fg=white,bold,bg=green' \
+    'touch' 'fg=white,bold,bg=green' \
+    'makenewdir' 'fg=white,bold,bg=green' \
     'mkdir' 'fg=white,bold,bg=green'
 )
 fi
