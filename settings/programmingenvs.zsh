81b38f5245bb2dab04dd4a945500d67db287f318
ashellwig <hellwigashton@gmail.com>
2017-10-02 23:17:16 -0600

    adding base16-shell

--- settings/programmingenvs.zsh
+++ settings/programmingenvs.zsh
@@ -9,3 +9,8 @@
 # --- NVM ---
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# --- Base16-Shell ---
+BASE16_SHELL="$HOME/.zsh/functions/base16-shell"
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$(
+  $BASE16_SHELL/profile_helper.sh
+  )"
