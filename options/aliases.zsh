2c2c9f54fd78a6d36abdb24c12885f15e6138a5f
ashellwig <hellwigashton@gmail.com>
2017-10-11 00:27:18 -0600

    updating aliases

--- options/aliases.zsh
+++ options/aliases.zsh
@@ -6,7 +6,7 @@
 
 # --- Colors ---
 # ls
-alias ls='ls -lFah --color=always'
+alias ls='ls -lFAh --color=always'
 alias newestdir="ls -ld *(/om[1])"
 # dmesg
 alias kernellogs="dmesg --human --color=always | most"
