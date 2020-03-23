e840eedac75f1c7b2f91a5093486d64431e7cda8
Ashton Hellwig <ahellwig.dev@gmail.com>
2020-03-23 04:02:55 -0600

    userdirs

--- options/aliases.zsh
+++ options/aliases.zsh
@@ -22,6 +22,10 @@ alias sce='sc enable'
 # Logs
 alias -g jc='sudo journalctl'
 alias kernellogs="dmesg --human --color=always --follow | most"
+# Config getters
+alias show_user_tex_config="cat ${HOME}/.zsh/settings/programmingenvs.zsh | sed -n -e '/#\s---\sLaTeX\s---/,/#\s---\sPython\s---/p' | sed 's/#\sexport/export/g' | grep -e '^export'"
+# SSH connections
+alias sshi='TERM=xterm-256color ssh -p22 $1'
 
 # --- Programs ---
 # Pacman
--- settings/xdg-user-dirs-settings.zsh
+++ settings/xdg-user-dirs-settings.zsh
@@ -9,6 +9,7 @@
 # --- Variables ---
 # XDG Base Directories
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DESKTOP_DIR="$HOME/Desktop"
 export XDG_DOWNLOAD_DIR="$HOME/Downloads"
 export XDG_TEMPLATES_DIR="$HOME/Templates"
