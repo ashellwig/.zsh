3fe6e53ddf57d8431d0a09941371b9de723b5937
Ashton Hellwig <ahellwig.dev@gmail.com>
2020-05-10 17:55:36 -0600

    add conda init function

--- settings/programmingenvs.zsh
+++ settings/programmingenvs.zsh
@@ -66,7 +66,22 @@ export GOPATH="$HOME/go"
 
 # --- Python ---
 # Conda
-source /home/ahellwig/anaconda3/etc/profile.d/conda.sh
+## Conda Initialize
+__conda_setup="$(
+  '/home/ahellwig/anaconda3/bin/conda' 'shell.zsh' 'hook' \
+    2> /dev/null
+)"
+
+if [ $? -eq 0 ]; then
+  eval "$__conda_setup"
+else
+  if [ -f "/home/ahellwig/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "/home/ahellwig/anaconda3/etc/profile.d/conda.sh"
+  else
+    export PATH="/home/ahellwig/anaconda3/bin:$PATH"
+  fi
+fi
+unset __conda_setup
 ## Conda Completions
 ### Load Completions
 fpath+=${ZDOTDIR}/functions/conda_zsh_completion
