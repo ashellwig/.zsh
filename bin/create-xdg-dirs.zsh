cf5c92dd8b9ba344c5eab0cf02e36daadcbbca61
Ash Hellwig <ahellwig.dev@gmail.com>
2024-06-10 02:49:53 -0600

    add script to generate XDG dirs

--- bin/create-xdg-dirs.zsh
+++ bin/create-xdg-dirs.zsh
@@ -1,4 +1,4 @@
-# vim: set et ft=zsh ts=2 sw=2
+#!/usr/bin/env zsh
 ####################################################
 # .zlogin                                         ##
 # @ashellwig                                      ##
@@ -18,3 +18,25 @@ export XDG_DOCUMENTS_DIR="$HOME/Documents"
 export XDG_MUSIC_DIR="$HOME/Music"
 export XDG_PICTURES_DIR="$HOME/Pictures"
 export XDG_VIDEOS_DIR="$HOME/Videos"
+
+typeset -a NEEDED_XDG_DIRS=(
+  "${XDG_CONFIG_HOME}"
+  "${XDG_DATA_HOME}"
+  "${XDG_DESKTOP_DIR}"
+  "${XDG_DOWNLOAD_DIR}"
+  "${XDG_TEMPLATES_DIR}"
+  "${XDG_PUBLICSHARE_DIR}"
+  "${XDG_DOCUMENTS_DIR}"
+  "${XDG_MUSIC_DIR}"
+  "${XDG_PICTURES_DIR}"
+  "${XDG_VIDEOS_DIR}"
+)
+
+for i in $NEEDED_XDG_DIRS; do
+  if [[ ! -d "${i}" ]]; then
+    echo -e "\033[1;31m${i} does not exist. Creating ${i}.\033[0m"
+    mkdir -p ${i}
+  else
+    echo -e "\033[1;32m${i} already exists. Skipping.\033[0m"
+  fi
+done
